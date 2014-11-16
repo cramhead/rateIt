806826e18ecd81eb25c6ca0507c397514070a5fe
Marc d'Entremont <cramhead@gmail.com>
2014-11-16 14:58:34 -0800

    Added loginButtons Added bootstrap3 styling Config Accounts

--- rateIt.js
+++ rateIt.js
@@ -3,21 +3,26 @@ if (Meteor.isClient) {
   Session.setDefault("counter", 0);
 
   Template.hello.helpers({
-    counter: function () {
+    counter: function() {
       return Session.get("counter");
     }
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click button': function() {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
     }
   });
+
+  // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
